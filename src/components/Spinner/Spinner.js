32525d42f092e8cdb6e3c754163beb689c536ee6
Nadezhda-Yarovaya <seriozz@yandex.ru>
2022-02-03 16:39:10 +0300

    0303 spinner and api photo

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,19 +1,18 @@
-import './spinner.css';
+import "./spinner.css";
 
 const Spinner = (props) => {
-    return (
-        <div className="spinner">
-            <div className="spinner__section"></div>
-            <div className="spinner__section"></div>
-            <div className="spinner__section"></div>
-            <div className="spinner__section"></div>
-            <div className="spinner__section"></div>
-            <div className="spinner__section"></div>
-            <div className="spinner__section"></div>
-            <div className="spinner__section"></div>
-            
-            </div>
-    );
-}
+  return (
+    <div className="spinner">
+      <div className="spinner__section"></div>
+      <div className="spinner__section"></div>
+      <div className="spinner__section"></div>
+      <div className="spinner__section"></div>
+      <div className="spinner__section"></div>
+      <div className="spinner__section"></div>
+      <div className="spinner__section"></div>
+      <div className="spinner__section"></div>
+    </div>
+  );
+};
 
 export default Spinner;
